73550d77b309108805e246f1e5e189e8af90a689
Marco Godínez <markotom@gmail.com>
2014-07-04 16:53:13 -0500

    Added new task to clone option-tree

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,36 @@
 'use strict';
 
 module.exports = function (grunt) {
-  
+
   grunt.initConfig({
-  
+
     // Watch task
     watch: {},
-  
+
     // Less task
-    less: {}
-  
+    less: {},
+
+    // Shell task
+    shell: {
+      optiontree: {
+        command: [
+          'rm -rf option-tree',
+          'git clone https://github.com/valendesigns/option-tree.git',
+        ].join('&&')
+      }
+    }
+
   });
-  
+
   // Load plugins
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-less');
-  
+  grunt.loadNpmTasks('grunt-shell');
+
+  // Build task
+  grunt.registerTask('build', ['shell:optiontree']);
+
+  // Default task
+  grunt.registerTask('default', 'build');
+
 };
