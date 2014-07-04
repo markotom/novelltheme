48ee608d81aa8690d82ce83bea234e48b88d7ff3
Marco Godínez <markotom@gmail.com>
2014-07-04 17:23:20 -0500

    Added functions.php

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+  if ( ! defined( 'ABSPATH' ) ) exit();
+
+  // Theme Options
+  // require get_template_directory() . '/includes/theme-options.php';
+
+  // Initialize
+  // require get_template_directory() . '/includes/initialize.php';
+
+  // Navigation
+  // require get_template_directory() . '/includes/navigation.php';
+
+?>
