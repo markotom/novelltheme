e2d7a3affbecc3266e1b2eec5613af6fcfd1591e
Marco Godínez <markotom@gmail.com>
2014-07-07 15:45:28 -0500

    Added footer with default contents and styles too

--- footer.php
+++ footer.php
@@ -1,3 +1,13 @@
+  </div><!-- /#content -->
+
+  <!-- #footer -->
+  <div id="footer">
+    <!-- .container -->
+    <div class="container">
+      <?php echo date( 'Y' ); ?> @ <?php bloginfo( 'sitename' ); ?>
+    </div><!-- /.container -->
+  </div><!-- /#footer -->
+
   <?php wp_footer(); ?>
 </body>
 </html>
