0042ec0369f6d236b2dc0300ab6864ac83d06878
Marco Godínez <markotom@gmail.com>
2014-07-10 23:08:37 -0500

    Added relationship to home url

--- header.php
+++ header.php
@@ -17,30 +17,44 @@
   <?php wp_head(); ?>
 </head>
 <body>
+
   <!-- #header -->
   <div id="header">
+
     <!-- .container -->
     <div class="container">
+
       <?php if ( ot_get_option( 'novell_logo' ) ) : ?>
+
+      <!-- .site-logo -->
       <div class="site-logo">
-        <a href="<?php echo home_url() ; ?>">
+        <a href="<?php echo home_url() ; ?>" rel="home">
           <img src="<?php echo ot_get_option( 'novell_logo' ) ?>" alt="">
         </a>
-      </div>
+      </div><!-- /.site-logo -->
+
       <?php elseif ( ot_get_option( 'novell_heading_text' ) ) : ?>
+
+      <!-- .site-name -->
       <h1 class="site-name">
-        <a href="<?php echo home_url() ; ?>">
+        <a href="<?php echo home_url() ; ?>" rel="home">
           <?php echo ot_get_option( 'novell_heading_text' ) ?>
         </a>
-      </h1>
+      </h1><!-- /.site-name -->
+
       <?php else : ?>
+
+      <!-- .site-name -->
       <h1 class="site-name">
-        <a href="<?php echo home_url() ; ?>">
+        <a href="<?php echo home_url() ; ?>" rel="home">
           <?php bloginfo( 'sitename' ) ?>
         </a>
-      </h1>
+      </h1><!-- /.site-name -->
+
       <?php endif ?>
+
     </div><!-- /.container -->
+
   </div><!-- /#header -->
 
   <!-- #content -->
