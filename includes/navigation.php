77ed7c1b5b0f3f85ed6c28a95036569c2e1b9617
Marco Godínez <markotom@gmail.com>
2014-07-11 03:14:25 -0500

    Added more config and support to dropdown menus

--- includes/navigation.php
+++ includes/navigation.php
@@ -10,10 +10,79 @@
 // After setup theme
 add_action( 'after_setup_theme', 'novell_register_nav_menus' );
 
+// Register Nav Menus
 function novell_register_nav_menus() {
 
   // Register nav main menu
   register_nav_menu( 'main', __( 'Main menu', 'novell' ) );
+
+}
+
+// Remove menu item id
+add_filter( 'nav_menu_item_id', '__return_null' );
+
+/**
+ * -----------------------------------------------------------------------------
+ * Dropdown menus
+ * -----------------------------------------------------------------------------
+ *
+ */
+
+// Modify menu item with walker
+add_filter( 'wp_nav_menu_args', 'novell_wp_nav_menu_args' );
+
+function novell_wp_nav_menu_args( $args ) {
+
+  // Add walker from Novell_Nav_Menu
+  $args[ 'walker' ] = new Novell_Nav_Menu();
+
+  return $args;
+
+}
+
+// Extends Walker_Nav_Menu to Novell_Nav_Menu
+// http://codex.wordpress.org/Class_Reference/Walker_Nav_Menu
+// http://wp.tutsplus.com/tutorials/creative-coding/understanding-the-walker-class/
+class Novell_Nav_Menu extends Walker_Nav_Menu {
+
+  function start_lvl( &$output, $depth = 0, $args = array() ) {
+
+    $output .= "\n<ul class=\"dropdown-menu\">\n";
+
+  }
+
+  function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+
+      $item_html = '';
+
+      parent::start_el( $item_html, $item, $depth, $args );
+
+      // Add classes and data attributes to support dropdown menus
+      if ( $item->is_dropdown && ( $depth === 0 ) ) {
+        $item_html = str_replace( '<a', '<a class="dropdown-toggle" data-toggle="dropdown" data-target="#"', $item_html );
+        $item_html = str_replace( '</a>', ' <b class="caret"></b></a>', $item_html );
+      }
+
+      // Remove link if is specified the "divider" class
+      if ( stristr( $item_html, 'li class="divider' ) ) {
+        $item_html = preg_replace( '/<a[^>]*>.*?<\/a>/iU', '', $item_html );
+      }
+
+      $output .= $item_html;
+
+    }
+
+    function display_element( $element, &$children_elements, $max_depth, $depth = 0, $args, &$output ) {
+
+      $element->is_dropdown = ! empty( $children_elements[ $element->ID ] );
+
+      if ( $element->is_dropdown ) {
+        $element->classes[] = 'dropdown';
+      }
+
+      parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
+
+    }
 
 }
 
