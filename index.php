cc195bd18c0a4f9c19c22d66f148540c404aa8c3
Marco Godínez <markotom@gmail.com>
2014-07-13 02:15:37 -0500

    Added column layouts based on twelve columns

--- index.php
+++ index.php
@@ -1,3 +1,59 @@
 <?php get_header(); ?>
-<div class="container">Content</div>
+<hr>
+
+<div class="container">
+  <div class="col-1-full">
+    <div class="content">Content</div>
+  </div>
+</div>
+
+<hr>
+
+<div class="container">
+  <div class="col-2-right">
+    <div class="content">Content</div>
+    <div class="sidebar-1">Sidebar 1</div>
+  </div>
+</div>
+
+<hr>
+
+<div class="container">
+  <div class="col-2-left">
+    <div class="content">Content</div>
+    <div class="sidebar-1">Sidebar 1</div>
+  </div>
+</div>
+
+<hr>
+
+<div class="container">
+  <div class="col-3-middle">
+    <div class="content">Content</div>
+    <div class="sidebar-1">Sidebar 1</div>
+    <div class="sidebar-2">Sidebar 2</div>
+  </div>
+</div>
+
+<hr>
+
+<div class="container">
+  <div class="col-3-right">
+    <div class="content">Content</div>
+    <div class="sidebar-1">Sidebar 1</div>
+    <div class="sidebar-2">Sidebar 2</div>
+  </div>
+</div>
+
+<hr>
+
+<div class="container">
+  <div class="col-3-left">
+    <div class="content">Content</div>
+    <div class="sidebar-1">Sidebar 1</div>
+    <div class="sidebar-2">Sidebar 2</div>
+  </div>
+</div>
+
+<hr>
 <?php get_footer(); ?>
