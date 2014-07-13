797ff9ab74940f6512b652bc020fde2002dc1314
Marco Godínez <markotom@gmail.com>
2014-07-12 20:44:51 -0500

    Added correctly main and bottom sidebar

--- sidebar-bottom.php
+++ sidebar-bottom.php
@@ -1,4 +1,11 @@
-<!-- .row -->
-<div class="row">
-  <?php dynamic_sidebar( 'bottom-sidebar' ); ?>
-</div><!-- /.row -->
+<?php if ( is_active_sidebar( 'bottom-sidebar' ) ) : ?>
+
+<!-- .sidebar.sidebar-bottom -->
+<div class="sidebar sidebar-bottom" role="complementary">
+  <!-- .row -->
+  <div class="row">
+    <?php dynamic_sidebar( 'bottom-sidebar' ); ?>
+  </div><!-- /.row -->
+</div><!-- /.sidebar.sidebar-bottom -->
+
+<?php endif; ?>
