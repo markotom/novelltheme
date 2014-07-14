062f63a62abd441b3d9060c49277b6288ddab7fc
Marco Godínez <markotom@gmail.com>
2014-07-14 01:59:32 -0500

    Added sidebars to default template

--- index.php
+++ index.php
@@ -19,28 +19,24 @@
     <?php
       // Hide main sidebar if specified full column
       if (  $column_global_layout !== 'col-1-full' ) :
+
+        // Get main sidebar
+        get_sidebar( 'main' );
+
+      endif;
     ?>
-
-    <!-- .sidebar.sidebar-main -->
-    <div class="sidebar sidebar-main" role="complementary">
-      Sidebar 1
-    </div><!-- /.sidebar.sidebar-main -->
-
-    <?php endif; ?>
 
     <?php
       // Show secondary sidebar if specified three columns
       if (  $column_global_layout === 'col-3-middle' ||
             $column_global_layout === 'col-3-right' ||
             $column_global_layout === 'col-3-left'   ) :
+
+        // Get secondary sidebar
+        get_sidebar( 'secondary' );
+
+      endif;
     ?>
-
-    <!-- .sidebar.sidebar-secondary -->
-    <div class="sidebar sidebar-secondary" role="complementary">
-      Sidebar 2
-    </div><!-- /.sidebar.sidebar-secondary -->
-
-    <?php endif; ?>
 
   </div><!-- /.col-(3|2|1)-(left|right|full) -->
 
