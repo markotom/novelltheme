31a8206b8cd0b74c0a683328697e1fcc19346bb4
Marco Godínez <markotom@gmail.com>
2014-07-30 17:57:23 -0500

    Added slides to carousel template

--- templates/carousel.php
+++ templates/carousel.php
@@ -1,32 +1,42 @@
-<div id="carousel-example-generic" class="carousel carousel-home slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-  </ol>
+<?php $slides = ot_get_option( 'novell_carousel_slides' ); ?>
 
-  <!-- Wrapper for slides -->
+<?php if ( is_array( $slides ) ) : ?>
+
+<!-- #carousel-home -->
+<div id="carousel-home" class="carousel carousel-home slide" data-ride="carousel">
+
+  <?php if ( count( $slides ) > 1 ) : ?>
+  <ol class="carousel-indicators">
+    <?php foreach ( $slides as $index => $slide ) : ?>
+    <li data-target="#carousel-home" data-slide-to="<?php echo $index ?>"<?php echo $index === 0 ? ' class="active"' : '' ?>></li>
+    <?php endforeach; ?>
+  </ol>
+  <?php endif; ?>
+
   <div class="carousel-inner">
-    <div class="item active">
-      <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzc3NyIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojNTU1O2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+Rmlyc3Qgc2xpZGU8L3RleHQ+PC9zdmc+" alt="...">
+    <?php foreach ( $slides as $index => $slide ) : ?>
+    <div class="item<?php echo $index === 0 ? ' active' : '' ?>" style="background-image: url(<?php echo $slide[ 'image' ] ?>)">
       <div class="carousel-caption">
-        ...
+        <h4>
+          <a href="<?php echo $slide[ 'url' ] ?>">
+            <?php echo $slide[ 'title' ] ?>
+          </a>
+        </h4>
+        <p><?php echo $slide[ 'caption' ] ?></p>
       </div>
     </div>
-    <div class="item">
-      <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzc3NyIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojNTU1O2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+Rmlyc3Qgc2xpZGU8L3RleHQ+PC9zdmc+" alt="...">
-      <div class="carousel-caption">
-        ...
-      </div>
-    </div>
+    <?php endforeach; ?>
   </div>
 
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+  <?php if ( count( $slides ) > 1 ) : ?>
+  <a class="left carousel-control" href="#carousel-home" role="button" data-slide="prev">
     <span class="glyphicon glyphicon-chevron-left"></span>
   </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+  <a class="right carousel-control" href="#carousel-home" role="button" data-slide="next">
     <span class="glyphicon glyphicon-chevron-right"></span>
   </a>
-</div>
+  <?php endif; ?>
+
+</div><!-- /#carousel-home -->
+
+<?php endif; ?>
