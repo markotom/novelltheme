ca09a3b691a8fbcffc1ddabd418080137f113c2c
Marco Godínez <markotom@gmail.com>
2014-08-04 21:54:25 -0500

    Added even or odd class to post classes

--- includes/content.php
+++ includes/content.php
@@ -14,4 +14,19 @@ function novell_excerpt_length() {
   return ot_get_option( 'novell_excerpt_length' );
 }
 
+// Filter post classes
+add_filter( 'post_class', 'novell_post_class' );
+
+function novell_post_class( $classes ) {
+
+  if ( ! is_single() ) {
+    global $wp_query;
+
+    // Set "odd" or "even" class if is not single
+    $classes[] = $wp_query->current_post % 2 == 0 ? 'even' : 'odd' ;
+  }
+
+  return $classes;
+}
+
 ?>
