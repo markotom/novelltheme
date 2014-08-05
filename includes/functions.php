cdbd5e95c0156552f5f3ba1595adbb090736298b
Marco Godínez <markotom@gmail.com>
2014-08-05 01:30:07 -0500

    Fixed to show sidebars by current layout

--- includes/functions.php
+++ includes/functions.php
@@ -91,7 +91,7 @@ function novell_current_layout() {
 function novell_main_sidebar() {
 
   // Hide main sidebar if specified full column
-  if (  novell_get_global_layout() !== 'col-1-full' ) :
+  if (  novell_get_current_layout() != 'col-1-full' ) :
 
     // Get main sidebar
     get_sidebar();
@@ -103,9 +103,9 @@ function novell_main_sidebar() {
 function novell_secondary_sidebar() {
 
   // Show secondary sidebar if specified three columns
-  if (  novell_get_global_layout() === 'col-3-middle' ||
-        novell_get_global_layout() === 'col-3-right' ||
-        novell_get_global_layout() === 'col-3-left'   ) :
+  if (  novell_get_current_layout() === 'col-3-middle' ||
+        novell_get_current_layout() === 'col-3-right' ||
+        novell_get_current_layout() === 'col-3-left'   ) :
 
     // Get secondary sidebar
     get_sidebar( 'secondary' );
