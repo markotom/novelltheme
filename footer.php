d8ee0e9b13b3877d33d4ddfda047203d70067aea
Marco Godínez <markotom@gmail.com>
2014-08-05 03:20:48 -0500

    Added CMS and theme credit

--- footer.php
+++ footer.php
@@ -16,7 +16,10 @@
       <?php if ( ot_get_option( 'novell_footer_text' ) ) : ?>
         <?php echo ot_get_option( 'novell_footer_text' ); ?>
       <?php else : ?>
-        <?php echo date( 'Y' ); ?> @ <?php bloginfo( 'sitename' ); ?>
+        <?php echo date( 'Y' ); ?> @ <?php bloginfo( 'sitename' ); ?>.
+        <br>
+        <?php _e( 'Powered by', 'novell' ); ?> <a href="http://wordpress.org" rel="nofollow">WordPress</a>.
+        <?php _e( 'Theme by', 'novell' ); ?> <a href="http://about.me/markotom">@Markotom</a>.
       <?php endif; ?>
     </div><!-- /.container -->
   </div><!-- /#footer -->
