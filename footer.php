79075dd13e0088e5c2284a9b0b69b4160c974c3c
Marco Godínez <markotom@gmail.com>
2014-08-05 01:53:28 -0500

    Added footer text and changed footer styles

--- footer.php
+++ footer.php
@@ -13,7 +13,11 @@
   <div id="footer">
     <!-- .container -->
     <div class="container">
-      <?php echo date( 'Y' ); ?> @ <?php bloginfo( 'sitename' ); ?>
+      <?php if ( ot_get_option( 'novell_footer_text' ) ) : ?>
+        <?php echo ot_get_option( 'novell_footer_text' ); ?>
+      <?php else : ?>
+        <?php echo date( 'Y' ); ?> @ <?php bloginfo( 'sitename' ); ?>
+      <?php endif; ?>
     </div><!-- /.container -->
   </div><!-- /#footer -->
 
